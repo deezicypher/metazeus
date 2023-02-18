176bb790f3f78f0c631dfd8dbc0685fb7bdb6ad6
deezitheviper <amuhdavid@gmail.com>
2023-02-18 18:35:35 +0100

    update on insights

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -19,6 +19,14 @@ const InsightCard = ({imgUrl, title, subtitle, index}) => (
         <h4 className="font-normal lg:text-[42px] text-[26px] text-secondary-white">{title}</h4>
         <p className="mt-[16px] font-normal lg:text-[20px] text-[15px] text-secondary-white">{subtitle}</p>
       </div>
+      <div className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-red-500">
+        <img 
+        src="arrow.svg"
+        alt="arrow"
+        className="w-[40%] h-[40%] object-contain"
+
+        />
+      </div>
     </div>
   </motion.div>
 );
