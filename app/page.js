04df35c3a96969c4609b7ba135f0a82fee540226
deezitheviper <amuhdavid@gmail.com>
2023-03-02 00:09:30 +0100

    build comfig

--- app/page.js
+++ app/page.js
@@ -10,12 +10,21 @@ const Page = () => (
     <div className='gradient-03 z-0' />
     <Explore />
     </div>
+    <div className='relative' id='about'>
     <GetStarted />
+    <div className='gradient-04 z-0' />
     <WhatsNew />
+    </div>
+    <div className='relative' id='about'>
     <World />
+    <div className='gradient-04 z-0' />
     <Insights />
+    </div>
+    <div className='relative' id='about'>
     <Feedback />
+    <div className='gradient-04 z-0' />
     <Footer />
+    </div>
   </div>
 );
 
