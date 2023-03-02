352c25ae7cf1ef3b3e4c6a3586e91f1b0dabf21a
deezitheviper <amuhdavid@gmail.com>
2023-03-02 12:49:37 +0100

    ignore eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   ],
   rules: {
     'react/no-unescaped-entities': 0,
+    "react/jsx-indent": 0,
     'eslintreact/no-danger': 0,
     'react/jsx-max-props-per-line': 0,
     'react/jsx-first-prop-new-line': 0,
--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,29 @@ import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Page = () => (
-  <div className='bg-primary-black overflow-hidden '>
-    <Navbar />
-    <Hero />
-    <div className='relative' id='about'>
-    <About />
-    <div className='gradient-03 z-0' />
-    <Explore />
-    </div>
-    <div className='relative' id='about'>
-    <GetStarted />
-    <div className='gradient-04 z-0' />
-    <WhatsNew />
-    </div>
-    <div className='relative' id='about'>
-    <World />
-    <div className='gradient-04 z-0' />
-    <Insights />
-    </div>
-    <div className='relative' id='about'>
-    <Feedback />
-    <div className='gradient-04 z-0' />
-    <Footer />
-    </div>
+  <div className="bg-primary-black overflow-hidden ">
+      <Navbar />
+      <Hero />
+      <div className="relative" id="about">
+      <About />
+      <div className="gradient-03 z-0" />
+      <Explore />
+      </div>
+      <div className="relative" id="about">
+      <GetStarted />
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
+      </div>
+      <div className="relative" id="about">
+      <World />
+      <div className="gradient-04 z-0" />
+      <Insights />
+      </div>
+      <div className="relative" id="about">
+      <Feedback />
+      <div className="gradient-04 z-0" />
+      <Footer />
+      </div>
   </div>
 );
 
